dc9ca1b0c993c1d936a6aa472d04efddd6c9b43c
George Santos <george.santos_ufmg@outlook.com>
2022-09-20 17:47:14 -0300

    feat: set Filters

---
+++ src/components/Filters.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Filters() {
+  return (
+    <form>
+      <fieldset>
+        <h1>Filtros</h1>
+      </fieldset>
+    </form>
+  );
+}
+
+export default Filters;
