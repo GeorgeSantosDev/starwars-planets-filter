6ecabfe4d655b827e068b29fefc59b59d3e19864
George Santos <george.santos_ufmg@outlook.com>
2022-09-20 21:40:28 -0300

    docs: Add new state

--- src/context/starWarsProvider.js
+++ src/context/starWarsProvider.js
@@ -3,17 +3,29 @@ import PropTypes from 'prop-types';
 import starWarsContext from './starWarsContext';
 
 function StarWarsProvider({ children }) {
-  const [searchedPlanets, setSearchedPlanets] = useState('');
-  const [filteredPlanets, setFilteredPlanets] = useState('');
+  const [searchedPlanets, setSearchedPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+
+  const [filters, setFilters] = useState({
+    filterByName: { name: '' } });
 
   const getPlanets = (array) => setSearchedPlanets(array);
   const setFilter = (array) => setFilteredPlanets(array);
+
+  const handleChangeName = ({ target }) => {
+    const { value } = target;
+    setFilters((prev) => ({
+      ...prev,
+      filterByName: { name: value } }));
+  };
 
   const contextValue = {
     searchedPlanets,
     getPlanets,
     filteredPlanets,
     setFilter,
+    filters,
+    handleChangeName,
   };
 
   return (
