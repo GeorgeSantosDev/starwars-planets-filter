80570524c31af25ba0bcdde59b228bc1d678c07a
George Santos <george.santos_ufmg@outlook.com>
2022-09-20 21:39:23 -0300

    docs: Add userEffect

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import starWarsContext from '../context/starWarsContext';
 
 function Filters() {
+  const { handleChangeName,
+    filters,
+    filteredPlanets,
+    setFilter,
+    searchedPlanets } = useContext(starWarsContext);
+
+  const { filterByName: { name } } = filters;
+
+  useEffect(() => {
+    if (name) {
+      const planetsFilterdByName = [...filteredPlanets].filter((planet) => planet.name
+        .includes(name));
+      setFilter(planetsFilterdByName);
+    } else {
+      setFilter(searchedPlanets);
+    }
+  }, [filters]);
+
   return (
     <form>
       <fieldset>
-        <h1>Filtros</h1>
+        <label htmlFor="filter-by-name">
+          <input
+            type="text"
+            id="filter-by-name"
+            data-testid="name-filter"
+            onChange={ (event) => handleChangeName(event) }
+            value={ name }
+          />
+        </label>
       </fieldset>
     </form>
   );
